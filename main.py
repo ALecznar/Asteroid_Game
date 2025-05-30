6f00b213e40c69a0c3234e36e274355d1d49d969
ALecznar <arondlecznar@gmail.com>
2025-05-29 21:59:41 -0400

    Screen size and opening and closing correctly

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+from constants import *
+
+
+
+def main():
+    pygame.init()  # Initialize pygame
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    black = (0,0,0)
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return # Exit the main function to close the window gracefully
+            
+        screen.fill(black, rect=None, special_flags=0)
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
